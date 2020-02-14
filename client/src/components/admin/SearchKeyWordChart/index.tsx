ea0ccf5d1347f0cab739a5e9f83b649814cd9d47
zhxuc <zhxuc7@163.com>
2020-02-14 13:51:53 +0800

    feat(client): update search keyword chart

--- client/src/components/admin/SearchKeyWordChart/index.tsx
+++ client/src/components/admin/SearchKeyWordChart/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useCallback, useEffect } from "react";
+import echarts from "echarts";
 import ReactEcharts from "echarts-for-react";
 import { SearchProvider } from "@/providers/search";
 
@@ -26,6 +27,8 @@ export const SearchKeyWordChart = () => {
   }, []);
 
   const dataX = data.map(d => d.keyword);
+  const dataY = data.map(d => d.count);
+  const yMax = Math.max.apply(null, dataY);
   const option = {
     tooltip: {},
     color: ["#3398DB"],
@@ -38,13 +41,13 @@ export const SearchKeyWordChart = () => {
     },
     dataZoom: [
       {
-        type: "inside",
-        start: 0,
-        end: 40
+        type: "inside"
+        // start: 0,
+        // end: data.length
       },
       {
-        start: 0,
-        end: 40,
+        // start: 0,
+        // end: data.length,
         handleIcon:
           "M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z",
         handleSize: "80%",
@@ -70,23 +73,34 @@ export const SearchKeyWordChart = () => {
     series: [
       {
         type: "bar",
-        // itemStyle: {
-        //   color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-        //     { offset: 0, color: "#83bff6" },
-        //     { offset: 0.5, color: "#188df0" },
-        //     { offset: 1, color: "#188df0" }
-        //   ])
-        // },
-        // emphasis: {
-        //   itemStyle: {
-        //     color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-        //       { offset: 0, color: "#2378f7" },
-        //       { offset: 0.7, color: "#2378f7" },
-        //       { offset: 1, color: "#83bff6" }
-        //     ])
-        //   }
-        // },
-        data: data.map(d => d.count)
+        itemStyle: {
+          color: "rgba(0,0,0,0.05)"
+        },
+        barGap: "-100%",
+        barCategoryGap: "40%",
+        data: dataY.map(() => yMax),
+        animation: false,
+        tooltip: { trigger: "none" }
+      },
+      {
+        type: "bar",
+        itemStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            { offset: 0, color: "#83bff6" },
+            { offset: 0.5, color: "#188df0" },
+            { offset: 1, color: "#188df0" }
+          ])
+        },
+        emphasis: {
+          itemStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              { offset: 0, color: "#2378f7" },
+              { offset: 0.7, color: "#2378f7" },
+              { offset: 1, color: "#83bff6" }
+            ])
+          }
+        },
+        data: dataY
       }
     ]
   };
