ab03fbb12d0d6a7e0c551a7823811edf75e5a6de
tingyun <tingyun@pinduoduo.com>
2020-03-18 12:30:33 +0800

    feat(admin): custom CKEditor

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -1,20 +1,21 @@
-const fs = require("fs");
-const path = require("path");
-const withPlugins = require("next-compose-plugins");
-const withCss = require("@zeit/next-css");
-const withSass = require("@zeit/next-sass");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const lessToJS = require("less-vars-to-js");
-const FilterWarningsPlugin = require("webpack-filter-warnings-plugin");
-const withAntd = require("./next-antd.config");
+const fs = require('fs');
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const withCss = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const withImages = require('next-images');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const lessToJS = require('less-vars-to-js');
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+const withAntd = require('./next-antd.config');
 
 const antdVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, "./src/theme/antd.less"), "utf8")
+  fs.readFileSync(path.resolve(__dirname, './src/theme/antd.less'), 'utf8')
 );
 
 // fix: prevents error when .less files are required by node
-if (typeof require !== "undefined") {
-  require.extensions[".less"] = file => {};
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = file => {};
 }
 
 const nextConfig = {
@@ -23,23 +24,22 @@ const nextConfig = {
     config.plugins.push(
       new FilterWarningsPlugin({
         // ignore ANTD chunk styles [mini-css-extract-plugin] warning
-        exclude: /mini-css-extract-plugin[^]*Conflicting order between:/
+        exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
       })
     );
     return config;
-  }
+  },
 };
 
 module.exports = withPlugins(
   [
     [withCss],
-
     [
       withSass,
       {
         cssModules: true,
-        localIdentName: "[name]__[local]_[hash:base64:5]"
-      }
+        localIdentName: '[name]__[local]_[hash:base64:5]',
+      },
     ],
     [
       withAntd,
@@ -47,14 +47,15 @@ module.exports = withPlugins(
         cssModules: true,
         cssLoaderOptions: {
           sourceMap: false,
-          importLoaders: 1
+          importLoaders: 1,
         },
         lessLoaderOptions: {
           javascriptEnabled: true,
-          modifyVars: antdVariables
-        }
-      }
-    ]
+          modifyVars: antdVariables,
+        },
+      },
+    ],
+    [withImages],
   ],
   nextConfig
 );
--- packages/admin/src/components/Editor/index.tsx
+++ packages/admin/src/components/Editor/index.tsx
@@ -1,15 +1,35 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import cls from 'classnames';
+import { FileProvider } from '@providers/file';
 import style from './index.module.scss';
-
-let CKEditor = (() => null) as any;
-let DecoupledEditor = () => null;
 
 interface IProps {
   value: string;
   onChange: (arg: any) => void;
   getToolbar?: (arg: any) => void;
 }
+
+// 自定义文件上传
+class MyUploadAdapter {
+  loader: any;
+
+  constructor(loader) {
+    this.loader = loader;
+  }
+
+  upload() {
+    return this.loader.file.then(file => FileProvider.uploadFile(file));
+  }
+}
+
+function MyCustomUploadAdapterPlugin(editor) {
+  editor.plugins.get('FileRepository').createUploadAdapter = loader => {
+    return new MyUploadAdapter(loader);
+  };
+}
+
+let CKEditor = (() => null) as any;
+let CustomEditor = () => null;
 
 export const Editor: React.FC<IProps> = ({ value, onChange, getToolbar }) => {
   const ref = useRef(null);
@@ -18,13 +38,10 @@ export const Editor: React.FC<IProps> = ({ value, onChange, getToolbar }) => {
   useEffect(() => {
     Promise.all([
       import('@ckeditor/ckeditor5-react'),
-      import('@ckeditor/ckeditor5-build-decoupled-document'),
-      import(
-        '@ckeditor/ckeditor5-build-decoupled-document/build/translations/zh-cn.js'
-      ),
+      import('./ckeditor/ckeditor'),
     ]).then(res => {
       CKEditor = res[0].default;
-      DecoupledEditor = res[1].default;
+      CustomEditor = res[1].default;
       setMounted(true);
     });
 
@@ -36,17 +53,17 @@ export const Editor: React.FC<IProps> = ({ value, onChange, getToolbar }) => {
   return mounted ? (
     <div className={cls(style.wrapper, '')} ref={ref}>
       <CKEditor
-        editor={DecoupledEditor}
+        editor={CustomEditor}
         data={value}
         onInit={editor => {
           getToolbar && getToolbar(editor.ui.view.toolbar.element);
         }}
-        onChange={(event, editor) => {
+        onChange={(_, editor) => {
           const data = editor.getData();
           onChange(data);
         }}
         config={{
-          language: 'zh-cn',
+          extraPlugins: [MyCustomUploadAdapterPlugin],
         }}
       />
     </div>
